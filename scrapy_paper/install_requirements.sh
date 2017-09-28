fc55de90dc732c973d3b0641d6059917654cfe64
CplusHua <hua.gong@qingteng.cn>
2017-09-28 18:56:37 +0800

    update requirements to sh

--- scrapy_paper/install_requirements.sh
+++ scrapy_paper/install_requirements.sh
@@ -6,5 +6,5 @@ pip install mysql-python
 pip install bs4
 
 # windows
-https://sourceforge.net/projects/pywin32/files/pywin32/Build%20221/
-download pywin32-221.win-amd64-py3.x.exe
+#https://sourceforge.net/projects/pywin32/files/pywin32/Build%20221/
+#download pywin32-221.win-amd64-py3.x.exe
